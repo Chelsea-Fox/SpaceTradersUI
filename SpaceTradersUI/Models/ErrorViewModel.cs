5a44f2780bdcc501516700926bf683e763bc220e
Chelsea <chelsea@foxtrt.com>
2025-01-23 12:00:57 +0000

    Base Project Created

---
+++ SpaceTradersUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpaceTradersUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
